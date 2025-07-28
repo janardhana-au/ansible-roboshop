a645178524f8129fa272213f1a135020ead617f4
janardhana-au <janardhana.au@gmail.com>
2025-07-28 22:57:46 +0930

    added

--- ansible-infra-testing/userdata.sh
+++ ansible-infra-testing/userdata.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+set -euxo pipefail
+
+# Install Docker
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
-systemctl enable docker
-usermod -aG docker ec2-user
-newgrp docker
-growpart /dev/nvme0n1 4
-lvextend -L +20G /dev/RootVG/rootVol
-lvextend -L +10G /dev/RootVG/varVol
 
-xfs_growfs /
-xfs_growfs /var
+# Start and enable Docker
+systemctl enable --now docker
+usermod -aG docker ec2-user
+
+# Expand disk and resize LVM
+growpart /dev/nvme0n1 4
+pvresize /dev/nvme0n1p4
+
+# Extend root and /var (you can adjust sizes or use +100%FREE if only one LV)
+lvextend -r -L +20G /dev/RootVG/rootVol
+lvextend -r -L +10G /dev/RootVG/varVol
